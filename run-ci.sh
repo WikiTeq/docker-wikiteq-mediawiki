692e963bb72eb03a253bcec7d08756a01edc3c47
Vedmaka <god.vedmaka@gmail.com>
2021-12-16 00:15:19 +0300

    CI save

--- run-ci.sh
+++ run-ci.sh
@@ -4,11 +4,10 @@ wget https://kojipkgs.fedoraproject.org//packages/sqlite/3.8.11/1.fc21/x86_64/sq
 yum -y install sqlite-3.8.11-1.fc21.x86_64.rpm
 composer update
 php maintenance/install.php --dbtype sqlite --dbuser root --dbname mw --dbpath $(pwd) --pass AdminPassword WikiName AdminUser
+echo 'error_reporting(0);' >> LocalSettings.php
 echo 'wfLoadExtension("Bootstrap");' >> LocalSettings.php
-echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
-echo '$wgShowDBErrorBacktrace = true;' >> LocalSettings.php
-echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
+echo '$wgShowExceptionDetails = false;' >> LocalSettings.php
+echo '$wgShowDBErrorBacktrace = false;' >> LocalSettings.php
+echo '$wgDevelopmentWarnings = false;' >> LocalSettings.php
 php maintenance/update.php --quick
-php tests/phpunit/phpunit.php --group skins-chameleon
-
-exit 1
+php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error
