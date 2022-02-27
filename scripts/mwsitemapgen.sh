679129f92896c69c91e979ea77d219f4336a698d
Vedmaka <god.vedmaka@gmail.com>
2022-02-27 18:28:34 +0300

    Updates server to handle protocol relative values

--- scripts/mwsitemapgen.sh
+++ scripts/mwsitemapgen.sh
@@ -10,6 +10,12 @@ fi
 # Convert to seconds (suffixed sleep command has issues on OSX)
 SLEEP_DAYS=$((MW_SITEMAP_PAUSE_DAYS * 60 * 60 * 24))
 
+SITE_SERVER=$WG_SITE_SERVER
+# Fallback to https:// scheme if it's protocol-relative
+if [[ $SITE_SERVER == "//"* ]]; then
+    SITE_SERVER="https:$SITE_SERVER"
+fi
+
 echo "Starting sitemap generator (in 30 seconds)..."
 # Wait three minutes after the server starts up to give other processes time to get started
 sleep 30
@@ -34,8 +40,8 @@ while true; do
       >> "$logfileNow" 2>&1
 
     # sending the sitemap to google
-    echo "sending to Google -> https://www.google.com/ping?sitemap=$WG_SITE_SERVER$WG_SCRIPT_PATH/sitemap/sitemap-index-mediawiki.xml"
-    curl --silent "https://www.google.com/ping?sitemap=$WG_SITE_SERVER$WG_SCRIPT_PATH/sitemap/sitemap-index-mediawiki.xml" > /dev/null
+    echo "sending to Google -> https://www.google.com/ping?sitemap=$SITE_SERVER$WG_SCRIPT_PATH/sitemap/sitemap-index-mediawiki.xml"
+    curl --silent "https://www.google.com/ping?sitemap=$SITE_SERVER$WG_SCRIPT_PATH/sitemap/sitemap-index-mediawiki.xml" > /dev/null
 
     # Wait some seconds to let the CPU do other things, like handling web requests, etc
     echo mwsitemapgen waits for "$SLEEP_DAYS" seconds... >> "$logfileNow"
